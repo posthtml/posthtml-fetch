8d77f456bbc2f887b32375a4063e0f740958aea0
Cosmin Popovici <hellocosmin@gmail.com>
2021-03-06 17:02:27 +0200

    style: fix linting errors

--- xo.config.js
+++ xo.config.js
@@ -3,6 +3,7 @@ module.exports = {
   space: true,
   rules: {
     semi: 0,
+    'unicorn/prefer-spread': 0,
     'unicorn/string-content': 0,
     'promise/prefer-await-to-then': 0,
     'unicorn/no-abusive-eslint-disable': 0,
