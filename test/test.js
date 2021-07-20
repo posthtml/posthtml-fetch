f764c4ae2095392123f372d3ed0cd7c12970ba10
Ivan Demidov <Scrum@list.ru>
2021-07-20 10:22:13 +0300

    build: update dep dev

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
-const test = require('ava')
-const plugin = require('../lib')
-const posthtml = require('posthtml')
-
 const path = require('path')
 const {readFileSync} = require('fs')
+const test = require('ava')
+const posthtml = require('posthtml')
+const plugin = require('../lib')
 
 const fixture = file => readFileSync(path.join(__dirname, 'fixtures', `${file}.html`), 'utf8')
 const expected = file => readFileSync(path.join(__dirname, 'expected', `${file}.html`), 'utf8')
@@ -11,39 +10,37 @@ const expected = file => readFileSync(path.join(__dirname, 'expected', `${file}.
 const error = (name, cb) => posthtml([plugin()]).process(fixture(name)).catch(cb)
 const clean = html => html.replace(/[^\S\r\n]+$/gm, '').trim()
 
-const process = (t, name, options, log = false) => {
-  return posthtml([plugin(options)])
-    .process(fixture(name))
-    .then(result => log ? console.log(result.html) : clean(result.html))
-    .then(html => t.is(html, expected(name).trim()))
-}
+const process = (t, name, options, log = false) => posthtml([plugin(options)])
+  .process(fixture(name))
+  .then(result => log ? console.log(result.html) : clean(result.html))
+  .then(html => t.is(html, expected(name).trim()))
 
-test('It renders the string if response is a string', t => {
-  return process(t, 'string')
+test('It renders the string if response is a string', async t => {
+  await process(t, 'string')
 })
 
-test('It works with object responses', t => {
-  return process(t, 'object')
+test('It works with object responses', async t => {
+  await process(t, 'object')
 })
 
-test('It works with custom tags', t => {
-  return process(t, 'tag', {tags: ['pull-in']})
+test('It works with custom tags', async t => {
+  await process(t, 'tag', {tags: ['pull-in']})
 })
 
-test('It skips node if attribute is missing', t => {
-  return process(t, 'no-attribute')
+test('It skips node if attribute is missing', async t => {
+  await process(t, 'no-attribute')
 })
 
-test('It skips node if src attribute is empty', t => {
-  return process(t, 'no-attribute-value')
+test('It skips node if src attribute is empty', async t => {
+  await process(t, 'no-attribute-value')
 })
 
-test('It works with expressions (loop)', t => {
-  return process(t, 'loop')
+test('It works with expressions (loop)', async t => {
+  await process(t, 'loop')
 })
 
-test('It works with options plugins after', t => {
-  return process(t, 'plugins-after', {
+test('It works with options plugins after', async t => {
+  await process(t, 'plugins-after', {
     preserveTag: true,
     plugins: {
       after(tree) {
@@ -51,15 +48,16 @@ test('It works with options plugins after', t => {
           if (typeof node === 'object') {
             node.attrs.after = '';
           }
+
           return node;
         })
-      }
-    }
+      },
+    },
   })
 })
 
-test('It works with options plugins before', t => {
-  return process(t, 'plugins-before', {
+test('It works with options plugins before', async t => {
+  await process(t, 'plugins-before', {
     preserveTag: true,
     plugins: {
       before(tree) {
@@ -67,25 +65,26 @@ test('It works with options plugins before', t => {
           if (typeof node === 'object') {
             node.attrs.before = '';
           }
+
           return node;
         })
 
         return tree;
-      }
-    }
+      },
+    },
   })
 })
 
-test('It fails if attribute contains an invalid URL', t => {
-  return error('invalid-src', err => {
-    t.is(err.message, 'Invalid URL: invalid')
+test('It fails if attribute contains an invalid URL', async t => {
+  await error('invalid-src', error_ => {
+    t.is(error_.message, 'Invalid URL: invalid')
   })
 })
 
-test('It uses options passed to got', t => {
-  return process(t, 'got-options', {got: {url: 'http://www.mocky.io/v2/5e837ecb3000008906cf3e9c'}})
+test('It uses options passed to got', async t => {
+  await process(t, 'got-options', {got: {url: 'http://www.mocky.io/v2/5e837ecb3000008906cf3e9c'}})
 })
 
-test('It works with custom attribute', t => {
-  return process(t, 'attribute', {attribute: 'from'})
+test('It works with custom attribute', async t => {
+  await process(t, 'attribute', {attribute: 'from'})
 })
